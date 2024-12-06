48729ad45dc337ab8f4c3e8bbdaea86c0f3ab56e
Petrauskas <povilas.petrauskas@cgi.com>
2024-12-06 09:40:43 +0200

    Adjusted jaeger tracer initializer code

--- jaeger.go
+++ jaeger.go
@@ -1,16 +1,17 @@
 package tracing
 
 import (
-	"io"
 	"log"
 	"os"
 
 	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-lib/metrics"
 )
 
 // InitJaeger initializes and returns a Jaeger tracer.
-func InitJaeger(serviceName string) (opentracing.Tracer, io.Closer) {
+func InitJaeger(serviceName string) (opentracing.Tracer, func()) {
 	jaegerHost := os.Getenv("JAEGER_AGENT_HOST")
 	if jaegerHost == "" {
 		jaegerHost = "jaeger" // The name of the Jaeger container in Docker Compose
@@ -19,17 +20,23 @@ func InitJaeger(serviceName string) (opentracing.Tracer, io.Closer) {
 	cfg := &config.Configuration{
 		ServiceName: serviceName,
 		Sampler: &config.SamplerConfig{
-			Type:  "const",
+			Type:  jaeger.SamplerTypeConst,
 			Param: 1, // Sample all traces
 		},
 		Reporter: &config.ReporterConfig{
+			LogSpans:           true,
 			LocalAgentHostPort: jaegerHost + ":6831", // Jaeger agent address
 		},
 	}
 
-	tracer, closer, err := cfg.NewTracer()
+	tracer, closer, err := cfg.NewTracer(
+		config.Logger(jaeger.StdLogger),
+		config.Metrics(metrics.NullFactory),
+	)
 	if err != nil {
 		log.Fatalf("Failed to initialize Jaeger: %v", err)
 	}
-	return tracer, closer
+	return tracer, func() {
+		_ = closer.Close()
+	}
 }
