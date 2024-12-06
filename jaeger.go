d543f358078889817727d7385def9829d849455b
Petrauskas <povilas.petrauskas@cgi.com>
2024-12-06 11:21:56 +0200

    Adjusted jaeger tracer initializer code

--- jaeger.go
+++ jaeger.go
@@ -7,7 +7,6 @@ import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
-	"github.com/uber/jaeger-lib/metrics"
 )
 
 // InitJaeger initializes and returns a Jaeger tracer.
@@ -24,15 +23,11 @@ func InitJaeger(serviceName string) (opentracing.Tracer, func()) {
 			Param: 1, // Sample all traces
 		},
 		Reporter: &config.ReporterConfig{
-			LogSpans:           true,
-			LocalAgentHostPort: jaegerHost + ":6831", // Jaeger agent address
+			LogSpans: true,
 		},
 	}
 
-	tracer, closer, err := cfg.NewTracer(
-		config.Logger(jaeger.StdLogger),
-		config.Metrics(metrics.NullFactory),
-	)
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
 	if err != nil {
 		log.Fatalf("Failed to initialize Jaeger: %v", err)
 	}
